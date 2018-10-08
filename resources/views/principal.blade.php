a76ae4063b9feae9acb0316fc35f1415f87591ec
Eder-Som <edersonoriza@gmail.com>
2018-10-08 10:33:22 -0300

    adicionando nova alteração

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .container {
+                text-align: center;
+                align-content: center;
+                background: #f7f7f7;
+            }
+
+
+        </style>
+    </head>
+    <body>
+
+            <div class="container">
+
+                <div class="panel panel-default">
+                    <h3>Projeto Tcc</h3>
+                    <h3>Cadastro de Alunos Creches do Municipio</h3>
+                </div>
+                </div>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+
+            </body>
+</html>
